0d312abb376892c2cb3d8671103011f9a73efbee
arumakan1727 <aluminumCan7271@gmail.com>
2021-01-25 01:30:05 +0900

    add: Easing 関数クラスを追加 #20

---
+++ src/Easing.java
@@ -0,0 +1,19 @@
+public final class Easing {
+    private Easing() {
+    }
+
+    public static double easeOutBounce(double x) {
+        final double n1 = 7.5625;
+        final double d1 = 2.75;
+
+        if (x < 1 / d1) {
+            return n1 * x * x;
+        } else if (x < 2 / d1) {
+            return n1 * (x -= 1.5 / d1) * x + 0.75;
+        } else if (x < 2.5 / d1) {
+            return n1 * (x -= 2.25 / d1) * x + 0.9375;
+        } else {
+            return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        }
+    }
+}
